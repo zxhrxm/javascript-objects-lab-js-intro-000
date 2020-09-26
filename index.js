82ffc78931df0d22a5238bc87329f94e29f02347
zahra.mahmood98 <zahra.mahmood98@hotmail.co.uk>
2020-09-26 13:48:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,6 +11,6 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 
 function deleteFromObjectByKey(object, key) {
   var newObj = Object.assign({}, object)
-  delete newObj.key;
+  delete newObj[key];
   return newObj
 }
